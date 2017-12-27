98b6531f0f7b7e34c83bda515bc99cba61f96367
linian365boy <linian365boy@foxmail.com>
2017-12-27 23:22:05 +0800

    feat(zuishare-member): 加评论功能

--- src/main/java/top/zuishare/Application.java
+++ src/main/java/top/zuishare/Application.java
@@ -1,10 +1,7 @@
 package top.zuishare;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 /**
  * @author niange
